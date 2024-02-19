24f30b3094e9b4a0efbc2ec81863ef3020f8fa23
TheNewRobot <amfloresa@uni.pe>
2024-02-19 19:56:52 +0000

    python script corrected

--- chatemis.py
+++ chatemis.py
@@ -1,7 +1,7 @@
 from os import system
 import speech_recognition as sr
 from playsound import playsound
-import gpt4all 
+from gpt4all import GPT4All
 import sys
 import whisper
 import warnings
@@ -10,3 +10,4 @@ import os
 
 wake_word = 'artemis'
 model = GPT4All('/root/.local/share/nomic.ai/GPT4All/gpt4all-falcon-newbpe-q4_0.gguf')
+breakpoint()
