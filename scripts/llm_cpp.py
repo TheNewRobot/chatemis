84083188e66560c4bc26fafc965910a016d52920
TheNewRobot <amfloresa@uni.pe>
2024-02-27 23:28:50 +0000

    scripts in classes tokenizer and llama_cpp

--- scripts/llm_cpp.py
+++ scripts/llm_cpp.py
@@ -1,4 +1,5 @@
 # from huggingface_hub import hf_hub_download
+import torch
 from langchain_community.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -8,65 +9,83 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 import yaml
 
-with open("../config.yaml", "r") as f:
-        config = yaml.safe_load(f)
+class LLM_object():
+    def __init__(self, config_path):
+        with open(config_path, "r") as f:
+                config = yaml.safe_load(f)
+        self.index_path = config['tokenizer']['db_faiss_path']
+        self.llm_model = config['llm_cpp']['model_path']
+        self.custom_prompt_template = config['llm_cpp']['custom_prompt_template']
+        self.n_gpu_layers = config['llm_cpp']['load_llm']['n_gpu_layers']
+        self.n_batch = config['llm_cpp']['load_llm']['n_batch']
+        self.max_tokens= config['llm_cpp']['load_llm']['max_tokens']
+        self.top_p = config['llm_cpp']['load_llm']['top_p']
+        self.repeat_penalty = config['llm_cpp']['load_llm']['repeat_penalty']
+        self.top_k = config['llm_cpp']['load_llm']['top_k']
+        self.n_ctx = config['llm_cpp']['load_llm']['n_ctx']
+        self.temperature = config['llm_cpp']['load_llm']['temperature']
+        self.verbose = config['llm_cpp']['load_llm']['verbose']
+        self.embeddings_context = config['tokenizer']['instructor_embeddings']
+        self.query = config['llm_cpp']['query']
+        self.device = []
 
-DB_FAISS_PATH = config['saving_tokens']['db_faiss_path']
-llm_model = config['llm_cpp']['model_path']
+    def check_cuda(self):
+        if torch.cuda.is_available():
+            device_name = torch.cuda.get_device_name(0)  
+            print("You are good to go with cuda! Device: ", device_name)
+            self.device = 'cuda'
+        else:
+            print("You are good to go with cpu!")
+            self.device = 'cpu'
 
-custom_prompt_template = """Use the following pieces of information to answer the user's question as a casual conversation
-Don't cite or mention any references from the text or figures. 
-Context: {context}
-Question: {question}
-Only return helpful and gramatically correct answers below.
-Helpful answer: """
+    def set_custom_prompt(self):
+        """
+        Prompt template for QA retrieval for each vectorstore
+        """
+        prompt = PromptTemplate(template=self.custom_prompt_template,
+                                input_variables=['context', 'question'])
+        return prompt
 
-def set_custom_prompt():
-    """
-    Prompt template for QA retrieval for each vectorstore
-    """
-    prompt = PromptTemplate(template=custom_prompt_template,
-                            input_variables=['context', 'question'])
-    return prompt
+    def retrieval_qa_chain(self,llm, prompt, db):
+        qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type='stuff',
+                                        retriever=db.as_retriever(search_kwargs={'k': 1}),
+                                        chain_type_kwargs={'prompt': prompt}
+                                        )
+        return qa_chain
 
-def retrieval_qa_chain(llm, prompt, db):
-    qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=db.as_retriever(search_kwargs={'k': 1}),
-                                       chain_type_kwargs={'prompt': prompt}
-                                       )
-    return qa_chain
+    def load_llm(self):
+        # Download the .gguf model that you want to implement and then used it
+        # It only works for gguf
+        llm = LlamaCpp(model_path=self.llm_model,
+                     n_gpu_layers=self.n_gpu_layers,
+                          n_batch=self.n_batch,
+                       max_tokens=self.max_tokens,
+                            top_p=self.top_p,
+                   repeat_penalty=self.repeat_penalty,
+                            top_k=self.top_k,
+                            n_ctx=self.n_ctx, 
+                      temperature=self.temperature,
+                          verbose=self.verbose)
+        return llm
 
-def load_llm():
-    # Download the .gguf model that you want to implement and then used it
-    # It only works for gguf
-    llm = LlamaCpp(model_path=config['llm_cpp']['model_path'],
-      n_gpu_layers=config['llm_cpp']['load_llm']['n_gpu_layers'],
-      n_batch=config['llm_cpp']['load_llm']['n_batch'],
-      max_tokens=config['llm_cpp']['load_llm']['max_tokens'],
-      top_p=config['llm_cpp']['load_llm']['top_p'],
-      repeat_penalty=config['llm_cpp']['load_llm']['repeat_penalty'],
-      top_k=config['llm_cpp']['load_llm']['top_k'],
-      n_ctx=config['llm_cpp']['load_llm']['n_ctx'], # Uncomment to increase the context window
-      temperature = config['llm_cpp']['load_llm']['temperature'],
-      verbose=config['llm_cpp']['load_llm']['verbose'],
-    )
-    return llm
+    def qa_bot(self):   
+        embeddings = HuggingFaceInstructEmbeddings(model_name=self.embeddings_context,
+                                            model_kwargs={'device': self.device})
+        db = FAISS.load_local(self.index_path, embeddings)
+        llm = self.load_llm()
+        qa_prompt = self.set_custom_prompt()
+        qa = self.retrieval_qa_chain(llm, qa_prompt, db)
+        return qa
 
-def qa_bot():   
-    embeddings = HuggingFaceInstructEmbeddings(model_name=config['saving_tokens']['instructor_embeddings'],
-                                        model_kwargs={'device': 'cuda'})
-    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
-    llm = load_llm()
-    qa_prompt = set_custom_prompt()
-    qa = retrieval_qa_chain(llm, qa_prompt, db)
-    return qa, llm
-
-def main():
-    qa, llm_handler = qa_bot()
+if __name__ == '__main__':
+    # Run this script as an example so you see how this nodes works
+    llama2 = LLM_object("../config.yaml")
+    llama2.check_cuda()
+    qa = llama2.qa_bot()
     print('We are using this model: ')
-    print(llm_model)
-    query = config['llm_cpp']['query']
+    print(llama2.llm_model)
+    query = llama2.query
     print("------------------")
     print(query)   
     print("------------------")
@@ -77,8 +96,6 @@ def main():
     print(response['result'])
     print("------------------")
 
-if __name__ == '__main__':
-    main()
 
 
  
---
+++ scripts/tokenizer.py
@@ -0,0 +1,62 @@
+# pip install langchain
+from langchain_community.document_loaders import DirectoryLoader # pip install "unstructured[local-inference]" This install torch 
+import torch
+from langchain.text_splitter import CharacterTextSplitter
+from InstructorEmbedding import INSTRUCTOR # pip install InstructorEmbedding #pip install sentence-transformers==2.2.2 (we should use this specific version)
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+import pickle
+from langchain_community.vectorstores import FAISS # pip install faiss_gpu 
+import yaml
+
+class Tokenizer:
+    def __init__(self, config_path):
+        with open(config_path, "r") as f:
+            config = yaml.safe_load(f)
+        self.data_path = config['tokenizer']['data_path']
+        self.index_path = config['tokenizer']['db_faiss_path']
+        self.separator = config['tokenizer']['text_spliter']['separator']
+        self.chunk_size = config['tokenizer']['text_spliter']['chunk_size']
+        self.chunk_overlap = config['tokenizer']['text_spliter']['chunk_overlap']
+        self.is_separator = config['tokenizer']['text_spliter']['is_separator']
+        self.model_name = config['tokenizer']['instructor_embeddings']
+        self.device = []
+
+    def check_cuda(self):
+        if torch.cuda.is_available():
+            device_name = torch.cuda.get_device_name(0)  
+            print("You are good to go with cuda! Device: ", device_name)
+            self.device = 'cuda'
+        else:
+            print("You are good to go with cpu!")
+            self.device = 'cpu'
+
+    def load_data(self):
+        loader = DirectoryLoader(self.data_path, glob="**/*.pdf", show_progress=True, use_multithreading=True)
+        docs = loader.load()
+        return docs
+
+    def split_into_chunks(self,docs):
+        text_splitter = CharacterTextSplitter(
+            separator= self.separator,
+            chunk_size= self.chunk_size,
+            chunk_overlap= self.chunk_overlap,
+            length_function=len,
+            is_separator_regex= self.is_separator ,
+        )
+        texts = text_splitter.split_documents(docs)
+        return texts
+
+    def store_embeddings(self,texts,device):
+        # Embedddings
+        instructor_embeddings = HuggingFaceInstructEmbeddings(model_name=self.model_name,
+                                                            model_kwargs={"device":device})
+        db = FAISS.from_documents(texts, instructor_embeddings)
+        db.save_local(self.index_path)
+
+if __name__=='__main__':
+    tokenizer = Tokenizer("../config.yaml")
+    tokenizer.check_cuda()
+    docs = tokenizer.load_data()
+    texts = tokenizer.split_into_chunks(docs)
+    tokenizer.store_embeddings(texts,tokenizer.device)
+    print("Documents loaded!")
