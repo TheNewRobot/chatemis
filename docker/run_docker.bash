6d4a4507bfbca5b28f88007b6e28cd6b4f9b1c58
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2025-03-08 17:49:48 -0800

    Update instructions for Dockerfile

--- docker/run_docker.bash
+++ docker/run_docker.bash
@@ -4,3 +4,4 @@ sudo docker run --rm -it \
 	--network=host \
 	chatemis
 
+#NOTE: You must add the line "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1" to the file /etc/pulse/default.pa for this to work properly. This only needs to be done once.
